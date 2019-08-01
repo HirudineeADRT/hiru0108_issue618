870cde90eeedb0ad1b91cd395223c0d51851a325
HirudineeADRT <hirudinee@adroitlogic.com>
2019-08-01 15:11:12 +0530

    added SNS

--- hiru0108_issue618/lambda.js
+++ hiru0108_issue618/lambda.js
@@ -1,4 +1,20 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
 exports.handler = function (event, context, callback) {
+    sns.publish({
+        Message: '123',
+        Subject: 'test',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
     //KTest1
     callback(null, { "message": "Successfully executed" });
 }
